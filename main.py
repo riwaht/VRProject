57059e63c2e852a61871d6d47569d71cf47cd84c
Riwa <riwa.hoteit@gmail.com>
2022-11-04 15:00:47 +0200

    Adding socket and turning data into string

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import socket
 
+
+
+# Parameters
+widthCam, heightCam = 1280, 720
 # Webcam
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
+cap.set(3, widthCam)
+cap.set(4, heightCam)
 
 # Hand Detector
 detector = HandDetector(maxHands = 1, detectionCon=0.8)
+
+# Communication
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5052)
 
 
 while True:
@@ -16,12 +25,19 @@ while True:
     # Find the hand and its landmarks
     hands, img = detector.findHands(img)
     
+    data = []
     # landmark values = [x,y,z] * 21
     if hands:
         # get the first hand detected
         hand1 = hands[0]
-        lmList1 = hand1["lmList"] # List of 21 Landmark points
-    
+        lmList1 = hand1['lmlist'] # List of 21 Landmark points
+        for lm in lmList1:
+             # append creates a separate list and appends the values in separate lines, we use extend to append the values in the same line
+             # in open cv, the x and y values are reversed so we need to reverse them (they are in top left corner, unity is in bottom right corner)
+             # we need to reverse the y value so that it is in the same direction as unity
+            data.extend([lm[0], heightCam - lm[1], lm[2]])
+    # send the data to unity
+    sock.sendTo(str.encode(str(data)), serverAddressPort)
     
     cv2.imshow("Image", img)
     cv2.waitKey(1)
